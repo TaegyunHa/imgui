4669317f115a26dd95bec930757413cc67dad917
Taegyun Ha <taegyunha92@gmail.com>
2022-06-12 11:18:46 +0100

    fix pch issue

--- premake5.lua
+++ premake5.lua
@@ -5,21 +5,19 @@ project "ImGui"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    pchheader "stdafx.h"
-    pchsource "%{prj.name}/src/stdafx.cpp"
-
     files
     {
-        "imconfig.h"
-        "imgui.h"
-        "imgui.cpp"
-        "imgui_draw.cpp"
-        "imgui_internal.h"
-        "imgui_widgets.cpp"
-        "imstb_rectpack.h"
-        "imstb_textedit.h"
-        "imstb_truetype.h"
-        "imgui_demo.cpp"
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+        "imgui_tables.cpp",
     }
 
     filter "system:windows"
